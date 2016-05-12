USA-RedDragon/Werewolf-hammerhead

Add branding, zip skeleton, and cleanup

build.sh
@@ -1,0 +1,42 @@
+#!/bin/bash
+
+export kernel=Werewolf
+export device=hammerhead
+export deviceconfig=werewolf_defconfig
+export outdir=/usr/share/nginx/html/Werewolf
+export makeopts="-j$(nproc)"
+export zImagePath="arch/arm/boot/zImage-dtb"
+export KBUILD_BUILD_USER=USA-RedDragon
+export KBUILD_BUILD_HOST=EdgeOfCreation
+export CROSS_COMPILE="ccache /root/deso/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-"
+export ARCH=arm
+
+export version=$(cat version)
+
+if [[ $1 =~ "clean" ]] ; then
+	make clean
+	make mrproper
+fi
+
+make ${makeopts} ${deviceconfig}
+make ${makeopts}
+
+if [ -a ${zImagePath} ] ; then
+	cp ${zImagePath} zip/zImage
+	find -name '*.ko' -exec cp -av {} zip//modules/ \;
+	cd zip
+	zip -q -r ${kernel}-${device}-${version}.zip anykernel.sh  META-INF  modules  ramdisk  tools zImage
+else
+	echo -e "\n\e[31m***** Build Failed *****\e[0m\n"
+fi
+
+if ! [ -d ${outdir} ] ; then
+	mkdir ${outdir}
+fi
+
+if [ -a ${kernel}-${device}-${version}.zip ] ; then
+	mv -v ${kernel}-${device}-${version}.zip ${outdir}
+fi
+
+rm -f kernel/zImage
+rm -f system/lib/modules/*

zip/anykernel.sh
@@ -1,0 +1,207 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=Werewolf Kernel
+do.devicecheck=1
+do.modules=1
+do.cleanup=1
+device.name1=hammerhead
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+
+chmod -R 755 $bin;
+mkdir -p $split_img;
+
+OUTFD=/proc/self/fd/$1;
+
+# ui_print <text>
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+
+# contains <string> <substring>
+contains() { test "${1#*$2}" != "$1" && return 0 || return 1; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
+  fi;
+  mv -f $ramdisk /tmp/anykernel/rdtmp;
+  mkdir -p $ramdisk;
+  cd $ramdisk;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+  if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
+    ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage ]; then
+    kernel=/tmp/anykernel/zImage;
+  else
+    kernel=`ls *-zImage`;
+    kernel=$split_img/$kernel;
+  fi;
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting..."; exit 1;
+  elif [ `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "New image larger than boot partition. Aborting..."; exit 1;
+  fi;
+  if [ -f "/data/custom_boot_image_patch.sh" ]; then
+    ash /data/custom_boot_image_patch.sh /tmp/anykernel/boot-new.img;
+    if [ $? != 0 ]; then
+      ui_print " "; ui_print "User script execution failed. Aborting..."; exit 1;
+    fi;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# replace_section <file> <begin search string> <end search string> <replacement string>
+replace_section() {
+  line=`grep -n "$2" $1 | cut -d: -f1`;
+  sed -i "/${2}/,/${3}/d" $1;
+  sed -i "${line}s;^;${4}\n;" $1;
+}
+
+# remove_section <file> <begin search string> <end search string>
+remove_section() {
+  sed -i "/${2}/,/${3}/d" $1;
+}
+
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5}\n;" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+insert_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;\n;" $1;
+    sed -i "$((line - 1))r $patch/$5" $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -pf $patch/$3 $1;
+  chmod $2 $1;
+}
+
+# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <original string> <replacement string>
+patch_fstab() {
+  entry=$(grep "$2" $1 | grep "$3");
+  if [ -z "$(echo "$entry" | grep "$6")" ]; then
+    case $4 in
+      block) part=$(echo "$entry" | awk '{ print $1 }');;
+      mount) part=$(echo "$entry" | awk '{ print $2 }');;
+      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
+      options) part=$(echo "$entry" | awk '{ print $4 }');;
+      flags) part=$(echo "$entry" | awk '{ print $5 }');;
+    esac;
+    newentry=$(echo "$entry" | sed "s;${part};${6};");
+    sed -i "s;${entry};${newentry};" $1;
+  fi;
+}
+
+## end methods
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
+## end install
+

USA-RedDragon/Werewolf-flo

Conflicts:
	AndroidKernel.mk
	arch/arm/configs/flo_defconfig
	arch/arm/configs/fsm9xxx-perf_defconfig
	arch/arm/configs/fsm9xxx_defconfig
	arch/arm/configs/full_msm8960-perf_defconfig
	arch/arm/configs/hammerhead_defconfig
	arch/arm/configs/msm7627a-perf_defconfig
	arch/arm/configs/msm7627a_defconfig
	arch/arm/configs/msm7630-perf_defconfig
	arch/arm/configs/msm7630_defconfig
	arch/arm/configs/msm8660-perf_defconfig
	arch/arm/configs/msm8660_defconfig
	arch/arm/configs/msm8960-perf_defconfig
	arch/arm/configs/msm8960_defconfig
	arch/arm/configs/msm8974_defconfig
	arch/arm/configs/msm9615_defconfig
	arch/arm/configs/msm9625_defconfig
	arch/arm/configs/werewolf_defconfig
	build.config
	make_defconfig.sh
	.gitignore
	build.sh

build.sh
@@ -1,83 +1,42 @@
 #!/bin/bash
-#
-#
-# Quanta Kernel build script
-#
-# How to use:
-# Compiling wihtout any arguments will compile the AOSP version
-# Compiling with cm or CM for an argument will compile the CM version
-#
-#
 
-export CROSS_COMPILE="$HOME/uber-arm-eabi-5.3/bin/arm-eabi-"
-KERNEL_DIRECTORY="$HOME/Quanta-Flo"
-ANYKERNEL_DIRECTORY="$HOME/anykernel_msm"
-JOBS=`grep -c "processor" /proc/cpuinfo`
-# Verify if the CM patches has already been applied. We don't want to apply them again if the compiling is stopped
-CM_CHECK=`grep -c "case MDP_YCBYCR_H2V1:" drivers/video/msm/mdp4_overlay.c`
-DEVICE="Flo"
-VERSION=8
+export kernel=Werewolf
+export device=flo
+export deviceconfig=werewolf_defconfig
+export outdir=/usr/share/nginx/html/Werewolf
+export makeopts="-j$(nproc)"
+export zImagePath="arch/arm/boot/zImage"
+export KBUILD_BUILD_USER=USA-RedDragon
+export KBUILD_BUILD_HOST=EdgeOfCreation
+export CROSS_COMPILE="ccache /root/deso/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9-uber/bin/arm-eabi-"
+export ARCH=arm
 
+export version=$(cat version)
 
-# Function responsible for download Anykernel if it's not found by the ANYKERNEL_DIRECTORY variable.
-function download_anykernel() {
-echo "Anykernel hasn't been found in $ANYKERNEL_DIRECTORY."
-echo "Downloading it..."
-git clone https://github.com/zaclimon/anykernel_msm -b flo-6.0 $ANYKERNEL_DIRECTORY
-} 
-
-# Download Anykernel if not found.
-if [ ! -d $ANYKERNEL_DIRECTORY ] ; then
-download_anykernel
+if [[ $1 =~ "clean" ]] ; then
+	make clean
+	make mrproper
 fi
 
-# Set the packaging information here. Specify if CM or AOSP.
-if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
-kernelzip="Quanta-V$VERSION-CM-$DEVICE.zip"
+make ${makeopts} ${deviceconfig}
+make ${makeopts}
 
-# Apply the CM patches if the variant specified is CM.
-if [ $CM_CHECK -eq 0 ] ; then
-git am CM/*
-fi
-
+if [ -a ${zImagePath} ] ; then
+	cp ${zImagePath} zip/zImage
+	find -name '*.ko' -exec cp -av {} zip//modules/ \;
+	cd zip
+	zip -q -r ${kernel}-${device}-${version}.zip anykernel.sh  META-INF  modules  ramdisk  tools zImage
 else
-kernelzip="Quanta-V$VERSION-$DEVICE.zip"
+	echo -e "\n\e[31m***** Build Failed *****\e[0m\n"
 fi
 
-# Ensure that we're on the correct Anykernel branch
-cd $ANYKERNEL_DIRECTORY
-git checkout flo-6.0
-
-cd $KERNEL_DIRECTORY
-
-# Create a /zip directory if not present
-mkdir -p ./zip
-
-# Clean the /zip directory if there have been a zip file before (Mostly a version of Quanta compiled.)
-if [ -e zip/*.zip ] ; then
-rm -rf zip/*
+if ! [ -d ${outdir} ] ; then
+	mkdir ${outdir}
 fi
 
-# Strangely, the kernel after compiling, auto-increments the version by 1. Let's revert that by decreasing the version value also by 1.
-let "VERSION -= 1"
-
-echo $VERSION > .version
-make quanta_defconfig
-make -j$JOBS
-
-# Copy the contents of the Anykernel folder if we confirm that a zImage is present.
-if [ -f arch/arm/boot/zImage ] ; then
-cd $ANYKERNEL_DIRECTORY
-cp -r * $KERNEL_DIRECTORY/zip/
-cd $KERNEL_DIRECTORY
-cp arch/arm/boot/zImage zip/tmp/anykernel
-
-# Remove the previously applied CM patches if we compiled with them before.
-if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
-git reset --hard HEAD~2
+if [ -a ${kernel}-${device}-${version}.zip ] ; then
+	mv -v ${kernel}-${device}-${version}.zip ${outdir}
 fi
- 
-cd zip/
-zip -r $kernelzip *
-echo "The kernel is situated at zip/$kernelzip"
-fi
+
+rm -f kernel/zImage
+rm -f system/lib/modules/*

zip/anykernel.sh
@@ -1,0 +1,207 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=Werewolf Kernel
+do.devicecheck=1
+do.modules=1
+do.cleanup=1
+device.name1=flo
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+
+# shell variables
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+
+chmod -R 755 $bin;
+mkdir -p $split_img;
+
+OUTFD=/proc/self/fd/$1;
+
+# ui_print <text>
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+
+# contains <string> <substring>
+contains() { test "${1#*$2}" != "$1" && return 0 || return 1; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
+  fi;
+  mv -f $ramdisk /tmp/anykernel/rdtmp;
+  mkdir -p $ramdisk;
+  cd $ramdisk;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+  if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
+    ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage ]; then
+    kernel=/tmp/anykernel/zImage;
+  else
+    kernel=`ls *-zImage`;
+    kernel=$split_img/$kernel;
+  fi;
+  cd $ramdisk;
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting..."; exit 1;
+  elif [ `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "New image larger than boot partition. Aborting..."; exit 1;
+  fi;
+  if [ -f "/data/custom_boot_image_patch.sh" ]; then
+    ash /data/custom_boot_image_patch.sh /tmp/anykernel/boot-new.img;
+    if [ $? != 0 ]; then
+      ui_print " "; ui_print "User script execution failed. Aborting..."; exit 1;
+    fi;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# replace_section <file> <begin search string> <end search string> <replacement string>
+replace_section() {
+  line=`grep -n "$2" $1 | cut -d: -f1`;
+  sed -i "/${2}/,/${3}/d" $1;
+  sed -i "${line}s;^;${4}\n;" $1;
+}
+
+# remove_section <file> <begin search string> <end search string>
+remove_section() {
+  sed -i "/${2}/,/${3}/d" $1;
+}
+
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5}\n;" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+insert_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;\n;" $1;
+    sed -i "$((line - 1))r $patch/$5" $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -pf $patch/$3 $1;
+  chmod $2 $1;
+}
+
+# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <original string> <replacement string>
+patch_fstab() {
+  entry=$(grep "$2" $1 | grep "$3");
+  if [ -z "$(echo "$entry" | grep "$6")" ]; then
+    case $4 in
+      block) part=$(echo "$entry" | awk '{ print $1 }');;
+      mount) part=$(echo "$entry" | awk '{ print $2 }');;
+      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
+      options) part=$(echo "$entry" | awk '{ print $4 }');;
+      flags) part=$(echo "$entry" | awk '{ print $5 }');;
+    esac;
+    newentry=$(echo "$entry" | sed "s;${part};${6};");
+    sed -i "s;${entry};${newentry};" $1;
+  fi;
+}
+
+## end methods
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
+## end install
+
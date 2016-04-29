4321597c05b63dad788a632fe923c4208f25a919
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-04-29 16:47:02 +0000

    Add branding, zip skeleton, and cleanup
    Conflicts:
    	AndroidKernel.mk
    	arch/arm/configs/flo_defconfig
    	arch/arm/configs/fsm9xxx-perf_defconfig
    	arch/arm/configs/fsm9xxx_defconfig
    	arch/arm/configs/full_msm8960-perf_defconfig
    	arch/arm/configs/hammerhead_defconfig
    	arch/arm/configs/msm7627a-perf_defconfig
    	arch/arm/configs/msm7627a_defconfig
    	arch/arm/configs/msm7630-perf_defconfig
    	arch/arm/configs/msm7630_defconfig
    	arch/arm/configs/msm8660-perf_defconfig
    	arch/arm/configs/msm8660_defconfig
    	arch/arm/configs/msm8960-perf_defconfig
    	arch/arm/configs/msm8960_defconfig
    	arch/arm/configs/msm8974_defconfig
    	arch/arm/configs/msm9615_defconfig
    	arch/arm/configs/msm9625_defconfig
    	arch/arm/configs/werewolf_defconfig
    	build.config
    	make_defconfig.sh
    
    Conflicts:
    	.gitignore
    	arch/arm/configs/werewolf_defconfig
    	build.sh

--- build.sh
+++ build.sh
@@ -1,83 +1,42 @@
 #!/bin/bash
-#
-#
-# Quanta Kernel build script
-#
-# How to use:
-# Compiling wihtout any arguments will compile the AOSP version
-# Compiling with cm or CM for an argument will compile the CM version
-#
-#
 
-export CROSS_COMPILE="$HOME/uber-arm-eabi-5.3/bin/arm-eabi-"
-KERNEL_DIRECTORY="$HOME/Quanta-Flo"
-ANYKERNEL_DIRECTORY="$HOME/anykernel_msm"
-JOBS=`grep -c "processor" /proc/cpuinfo`
-# Verify if the CM patches has already been applied. We don't want to apply them again if the compiling is stopped
-CM_CHECK=`grep -c "case MDP_YCBYCR_H2V1:" drivers/video/msm/mdp4_overlay.c`
-DEVICE="Flo"
-VERSION=8
+export kernel=Werewolf
+export device=flo
+export deviceconfig=werewolf_defconfig
+export outdir=/usr/share/nginx/html/Werewolf
+export makeopts="-j$(nproc)"
+export zImagePath="arch/arm/boot/zImage"
+export KBUILD_BUILD_USER=USA-RedDragon
+export KBUILD_BUILD_HOST=EdgeOfCreation
+export CROSS_COMPILE="ccache /root/deso/prebuilts/gcc/linux-x86/arm/arm-eabi-4.9-uber/bin/arm-eabi-"
+export ARCH=arm
 
+export version=$(cat version)
 
-# Function responsible for download Anykernel if it's not found by the ANYKERNEL_DIRECTORY variable.
-function download_anykernel() {
-echo "Anykernel hasn't been found in $ANYKERNEL_DIRECTORY."
-echo "Downloading it..."
-git clone https://github.com/zaclimon/anykernel_msm -b flo-6.0 $ANYKERNEL_DIRECTORY
-} 
-
-# Download Anykernel if not found.
-if [ ! -d $ANYKERNEL_DIRECTORY ] ; then
-download_anykernel
+if [[ $1 =~ "clean" ]] ; then
+	make clean
+	make mrproper
 fi
 
-# Set the packaging information here. Specify if CM or AOSP.
-if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
-kernelzip="Quanta-V$VERSION-CM-$DEVICE.zip"
+make ${makeopts} ${deviceconfig}
+make ${makeopts}
 
-# Apply the CM patches if the variant specified is CM.
-if [ $CM_CHECK -eq 0 ] ; then
-git am CM/*
-fi
-
+if [ -a ${zImagePath} ] ; then
+	cp ${zImagePath} zip/zImage
+	find -name '*.ko' -exec cp -av {} zip//modules/ \;
+	cd zip
+	zip -q -r ${kernel}-${device}-${version}.zip anykernel.sh  META-INF  modules  ramdisk  tools zImage
 else
-kernelzip="Quanta-V$VERSION-$DEVICE.zip"
+	echo -e "\n\e[31m***** Build Failed *****\e[0m\n"
 fi
 
-# Ensure that we're on the correct Anykernel branch
-cd $ANYKERNEL_DIRECTORY
-git checkout flo-6.0
-
-cd $KERNEL_DIRECTORY
-
-# Create a /zip directory if not present
-mkdir -p ./zip
-
-# Clean the /zip directory if there have been a zip file before (Mostly a version of Quanta compiled.)
-if [ -e zip/*.zip ] ; then
-rm -rf zip/*
+if ! [ -d ${outdir} ] ; then
+	mkdir ${outdir}
 fi
 
-# Strangely, the kernel after compiling, auto-increments the version by 1. Let's revert that by decreasing the version value also by 1.
-let "VERSION -= 1"
-
-echo $VERSION > .version
-make quanta_defconfig
-make -j$JOBS
-
-# Copy the contents of the Anykernel folder if we confirm that a zImage is present.
-if [ -f arch/arm/boot/zImage ] ; then
-cd $ANYKERNEL_DIRECTORY
-cp -r * $KERNEL_DIRECTORY/zip/
-cd $KERNEL_DIRECTORY
-cp arch/arm/boot/zImage zip/tmp/anykernel
-
-# Remove the previously applied CM patches if we compiled with them before.
-if [[ "$1" =~ "cm" || "$1" =~ "CM" ]] ; then
-git reset --hard HEAD~2
+if [ -a ${kernel}-${device}-${version}.zip ] ; then
+	mv -v ${kernel}-${device}-${version}.zip ${outdir}
 fi
- 
-cd zip/
-zip -r $kernelzip *
-echo "The kernel is situated at zip/$kernelzip"
-fi
+
+rm -f kernel/zImage
+rm -f system/lib/modules/*
